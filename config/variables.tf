906ac0c2ae5c9cacea917e303e63e71947eb39bc
John Stockbauer <john@shippable.com>
2016-01-19 14:24:39 -0800

    new variable subs

--- config/variables.tf
+++ config/variables.tf
@@ -17,3 +17,7 @@ variable "cluster_name" {
   description = "the name of the cluster created on the provider"
   default = "trriplejay-test"
 }
+
+variable "JSON_key" {
+  description = "credentials for accessing GCP.  Shippable will set a value in the ENV."
+}
