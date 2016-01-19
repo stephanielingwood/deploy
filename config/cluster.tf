57c9f27694e5c1f9f5cf7eeea755f6649ebce3eb
John Stockbauer <john@shippable.com>
2016-01-19 13:10:27 -0800

    add a machine_type

--- config/cluster.tf
+++ config/cluster.tf
@@ -1,12 +1,16 @@
 # GKE clusters
 resource "google_container_cluster" "trriplejayCL" {
-    name = "${var.cluster_name}"
-    zone = "${var.availability_zone}"
-    initial_node_count = 3
+  name = "${var.cluster_name}"
+  zone = "${var.availability_zone}"
+  initial_node_count = 3
 
-    master_auth {
-        username = "mr.yoda"
-        password = "adoy.rm"
-    }
+  master_auth {
+    username = "mr.yoda"
+    password = "adoy.rm"
+  }
+
+  node_config {
+    machine_type = "f1-micro"
+  }
 
 }
